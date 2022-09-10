c04ba367941a9fc617350c756830b65676c478a0
Rodrigo <rodrigosedano@me.com>
2022-09-10 15:56:13 +0200

    Fix getting steam PID

--- src/renderer/pages/EndPage.js
+++ src/renderer/pages/EndPage.js
@@ -339,7 +339,7 @@ const EndPage = () => {
 
   const openSRM = () => {
     ipcChannel.sendMessage('bash', [
-      `zenity --question --width 450 --title \"Close Steam/Steam Input?\" --text \"Exit Steam to launch Steam Rom Manager? Desktop controls will temporarily revert to touch/trackpad/L2/R2\" && (kill -15 $(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
+      `zenity --question --width 450 --title \"Close Steam/Steam Input?\" --text \"Exit Steam to launch Steam Rom Manager? Desktop controls will temporarily revert to touch/trackpad/L2/R2\" && (kill -15 \$(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
     ]);
 
 
