d1b67134813741bd8ae23f90a2794d85baed0300
Rodrigo <rodrigosedano@me.com>
2022-09-17 12:19:56 +0200

    differente SRM Parser for RemotePlay

--- src/renderer/pages/RemotePlayWhateverPage.js
+++ src/renderer/pages/RemotePlayWhateverPage.js
@@ -27,7 +27,7 @@ const RemotePlayWhateverPage = () => {
 
   const openSRM = () => {
     ipcChannel.sendMessage('bash', [
-      `zenity --question --width 450 --title \"Close Steam/Steam Input?\" --text \"Exit Steam to launch Steam Rom Manager? Desktop controls will temporarily revert to touch/trackpad/L2/R2\" && (kill -15 \$(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
+      `zenity --question --width 450 --title \"Close Steam/Steam Input?\" --text \"Exit Steam to launch Steam Rom Manager? Desktop controls will temporarily revert to touch/trackpad/L2/R2\" && (kill -15 \$(pidof steam) && cp $HOME/emudeck/backend/configs/steam-rom-manager/userData/userConfigurationsRPW.json $HOME/.config/steam-rom-manager/userData/userConfigurations.json & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage && cp $HOME/emudeck/backend/configs/steam-rom-manager/userData/userConfigurations.json $HOME/.config/steam-rom-manager/userData/userConfigurations.json)`,
     ]);
   };
 
