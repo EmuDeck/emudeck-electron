51305577e2ad418a532e87fa9aa5dcae584f6fff
Rodrigo <rodrigosedano@me.com>
2022-10-17 18:39:56 +0200

    emudeck-nolog

--- src/main/main.ts
+++ src/main/main.ts
@@ -121,6 +121,32 @@ ipcMain.on('emudeck', async (event, command) => {
   );
 });
 
+ipcMain.on('emudeck-nolog', async (event, command) => {
+  let backChannel;
+  let bashCommand;
+
+  if (command[0].includes('|||')) {
+    const tempCommand = command[0].split('|||');
+    backChannel = tempCommand[0];
+    bashCommand = tempCommand[1];
+  } else {
+    backChannel = 'none';
+    bashCommand = command;
+  }
+
+  return exec(
+    `source ~/.config/EmuDeck/backend/functions/all.sh && ${bashCommand}`,
+    (error, stdout, stderr) => {
+      //event.reply('console', { backChannel });
+      event.reply(backChannel, {
+        stdout: stdout,
+        stderr: stderr,
+        error: error,
+      });
+    }
+  );
+});
+
 ipcMain.on('clean-log', async (event, command) => {
   exec(`echo "[$(date)] App Installed" > $HOME/emudeck/Emudeck.AppImage.log`);
 });
