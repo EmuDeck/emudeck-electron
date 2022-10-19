858d28ad7de61194fa773dc174231845988c987a
Rodrigo <rodrigosedano@me.com>
2022-10-19 17:22:19 +0200

    Agnostic Emulators

--- src/renderer/pages/EmulatorSelectorPage.js
+++ src/renderer/pages/EmulatorSelectorPage.js
@@ -1,7 +1,43 @@
-import React, { useEffect, useState, useContext } from "react";
-import { GlobalContext } from "context/globalContext";
+import React, { useEffect, useState, useContext } from 'react';
+import { GlobalContext } from 'context/globalContext';
 
-import EmulatorSelector from "components/organisms/Wrappers/EmulatorSelector.js";
+import EmulatorSelector from 'components/organisms/Wrappers/EmulatorSelector.js';
+
+import imgra from 'assets/emulators/ra.png';
+import imgdolphin from 'assets/emulators/dolphin.png';
+import imgprimehacks from 'assets/emulators/primehacks.png';
+import imgppsspp from 'assets/emulators/ppsspp.png';
+import imgduckstation from 'assets/emulators/duckstation.png';
+import imgcitra from 'assets/emulators/citra.png';
+import imgpcsx2 from 'assets/emulators/pcsx2.png';
+import imgrpcs3 from 'assets/emulators/rpcs3.png';
+import imgyuzu from 'assets/emulators/yuzu.png';
+import imgryujinx from 'assets/emulators/ryujinx.png';
+import imgcemu from 'assets/emulators/cemu.png';
+import imgxemu from 'assets/emulators/xemu.png';
+import imgmame from 'assets/emulators/mame.png';
+import imgvita3k from 'assets/emulators/vita3k.png';
+import imgscummvm from 'assets/emulators/scummvm.png';
+import imgsupermodelista from 'assets/emulators/supermodelista.png';
+
+const images = {
+  ra: imgra,
+  dolphin: imgdolphin,
+  primehacks: imgprimehacks,
+  ppsspp: imgppsspp,
+  duckstation: imgduckstation,
+  citra: imgcitra,
+  pcsx2: imgpcsx2,
+  rpcs3: imgrpcs3,
+  yuzu: imgyuzu,
+  ryujinx: imgryujinx,
+  cemu: imgcemu,
+  xemu: imgxemu,
+  mame: imgmame,
+  vita3k: imgvita3k,
+  scummvm: imgscummvm,
+  supermodelista: imgsupermodelista,
+};
 
 const EmulatorSelectorPage = () => {
   const { state, setState } = useContext(GlobalContext);
@@ -10,7 +46,7 @@ const EmulatorSelectorPage = () => {
   const [statePage, setStatePage] = useState({
     disabledNext: false,
     disabledBack: false,
-    data: "",
+    data: '',
   });
   const { disabledNext, disabledBack, data } = statePage;
 
@@ -32,6 +68,7 @@ const EmulatorSelectorPage = () => {
       onClick={toggleEmus}
       disabledNext={disabledNext}
       disabledBack={disabledBack}
+      images={images}
     />
   );
 };
