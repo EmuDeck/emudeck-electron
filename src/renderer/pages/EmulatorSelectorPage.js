04745e23527f9401c5bdfa63dbf8bbdb1d0bf6ba
Rodrigo <rodrigosedano@me.com>
2023-01-14 11:59:54 +0100

    Some GUI Fixes

--- src/renderer/pages/EmulatorSelectorPage.js
+++ src/renderer/pages/EmulatorSelectorPage.js
@@ -3,25 +3,30 @@ import { GlobalContext } from 'context/globalContext';
 
 import EmulatorSelector from 'components/organisms/Wrappers/EmulatorSelector.js';
 
-import imgra from 'assets/emulators/ra.png';
-import imgdolphin from 'assets/emulators/dolphin.png';
-import imgprimehacks from 'assets/emulators/primehacks.png';
-import imgppsspp from 'assets/emulators/ppsspp.png';
-import imgduckstation from 'assets/emulators/duckstation.png';
-import imgmelonds from 'assets/emulators/melonds.png';
-import imgcitra from 'assets/emulators/citra.png';
-import imgpcsx2 from 'assets/emulators/pcsx2.png';
-import imgrpcs3 from 'assets/emulators/rpcs3.png';
-import imgyuzu from 'assets/emulators/yuzu.png';
-import imgryujinx from 'assets/emulators/ryujinx.png';
-import imgrmg from 'assets/emulators/rmg.png';
-import imgcemu from 'assets/emulators/cemu.png';
-import imgxemu from 'assets/emulators/xemu.png';
-import imgmame from 'assets/emulators/mame.png';
-import imgvita3k from 'assets/emulators/vita3k.png';
-import imgscummvm from 'assets/emulators/scummvm.png';
-import imgsupermodelista from 'assets/emulators/supermodelista.png';
-import imgesde from 'assets/emulators/esde.png';
+import {
+  imgdefault,
+  imgra,
+  imgdolphin,
+  imgprimehacks,
+  imgppsspp,
+  imgduckstation,
+  imgcitra,
+  imgpcsx2,
+  imgrpcs3,
+  imgyuzu,
+  imgryujinx,
+  imgcemu,
+  imgxemu,
+  imgmame,
+  imgvita3k,
+  imgxenia,
+  imgsrm,
+  imgrmg,
+  imgscummvm,
+  imgsupermodelista,
+  imgesde,
+  imgmelonds,
+} from 'components/utils/images/images.js';
 
 const images = {
   ra: imgra,
