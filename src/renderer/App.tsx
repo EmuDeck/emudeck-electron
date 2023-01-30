4daaf229475fa9b0a5b8dfebf52af59a9110dacf
Rodrigo <rodrigosedano@me.com>
2023-01-30 10:38:02 +0100

    StoreUI WIP

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -41,6 +41,7 @@ import EmuGuidePage from 'pages/EmuGuidePage';
 import AutoSavePage from 'pages/AutoSavePage';
 import HomebrewGamesPage from 'pages/HomebrewGamesPage';
 import ConfirmationPage from 'pages/ConfirmationPage';
+import StoreFrontPage from 'pages/StoreFrontPage';
 import EmulatorResolutionPage from 'pages/EmulatorResolutionPage';
 
 import EndPage from 'pages/EndPage';
@@ -248,6 +249,7 @@ export default function App() {
           <Route exact path="/auto-save" element={<AutoSavePage />} />
           <Route exact path="/homebrew-games" element={<HomebrewGamesPage />} />
           <Route exact path="/confirmation" element={<ConfirmationPage />} />
+          <Route exact path="/store-front" element={<StoreFrontPage />} />
 
           <Route
             exact
