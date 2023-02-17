3a1660cac50751c45614524a50f7fb6f6ad2a912
Rodrigo <rodrigosedano@me.com>
2023-02-17 12:45:55 +0100

    win10fix

--- src/renderer/pages/EndPage.jsx
+++ src/renderer/pages/EndPage.jsx
@@ -85,7 +85,7 @@ const EndPage = () => {
     const interval = setInterval(() => {
       if (system === 'win32') {
         let msg = readMSG(
-          'more "%userprofile%\\AppData\\Roaming\\EmuDeck\\msg.log"'
+          'more $env:USERPROFILE/AppData/Roaming/EmuDeck/msg.log'
         );
       } else {
         let msg = readMSG('cat ~/.config/EmuDeck/msg.log');
