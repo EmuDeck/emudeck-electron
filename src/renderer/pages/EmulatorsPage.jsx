593ff5ff754b2905739fd3dc9886d9358c1bc363
Rodrigo <rodrigosedano@me.com>
2023-04-23 22:58:44 +0200

    Uninstall fix

--- src/renderer/pages/EmulatorsPage.jsx
+++ src/renderer/pages/EmulatorsPage.jsx
@@ -91,116 +91,6 @@ function EmulatorsPage() {
 
   const ipcChannel = window.electron.ipcRenderer;
 
-  const diff = (obj1, obj2) => {
-    // Make sure an object to compare is provided
-    if (!obj2 || Object.prototype.toString.call(obj2) !== '[object Object]') {
-      return obj1;
-    }
-
-    //
-    // Variables
-    //
-
-    const diffs = {};
-    let key;
-
-    //
-    // Methods
-    //
-
-    /**
-     * Check if two arrays are equal
-     * @param  {Array}   arr1 The first array
-     * @param  {Array}   arr2 The second array
-     * @return {Boolean}      If true, both arrays are equal
-     */
-    const arraysMatch = (arr1, arr2) => {
-      // Check if the arrays are the same length
-      if (arr1.length !== arr2.length) return false;
-
-      // Check if all items exist and are in the same order
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false;
-      }
-
-      // Otherwise, return true
-      return true;
-    };
-
-    /**
-     * Compare two items and push non-matches to object
-     * @param  {*}      item1 The first item
-     * @param  {*}      item2 The second item
-     * @param  {String} key   The key in our object
-     */
-    const compare = (item1, item2, key) => {
-      // Get the object type
-      const type1 = Object.prototype.toString.call(item1);
-      const type2 = Object.prototype.toString.call(item2);
-
-      // If type2 is undefined it has been removed
-      if (type2 === '[object Undefined]') {
-        diffs[key] = null;
-        return;
-      }
-
-      // If items are different types
-      if (type1 !== type2) {
-        diffs[key] = item2;
-        return;
-      }
-
-      // If an object, compare recursively
-      if (type1 === '[object Object]') {
-        const objDiff = diff(item1, item2);
-        if (Object.keys(objDiff).length > 0) {
-          diffs[key] = objDiff;
-        }
-        return;
-      }
-
-      // If an array, compare
-      if (type1 === '[object Array]') {
-        if (!arraysMatch(item1, item2)) {
-          diffs[key] = item2;
-        }
-        return;
-      }
-
-      // Else if it's a function, convert to a string and compare
-      // Otherwise, just compare
-      if (type1 === '[object Function]') {
-        if (item1.toString() !== item2.toString()) {
-          diffs[key] = item2;
-        }
-      } else if (item1 !== item2) {
-        diffs[key] = item2;
-      }
-    };
-
-    //
-    // Compare our objects
-    //
-
-    // Loop through the first object
-    for (key in obj1) {
-      if (obj1.hasOwnProperty(key)) {
-        compare(obj1[key], obj2[key], key);
-      }
-    }
-
-    // Loop through the second object and find missing items
-    for (key in obj2) {
-      if (obj2.hasOwnProperty(key)) {
-        if (!obj1[key] && obj1[key] !== obj2[key]) {
-          diffs[key] = obj2[key];
-        }
-      }
-    }
-
-    // Return the object of differences
-    return diffs;
-  };
   const countRef = useRef(stateCurrentConfigs);
   countRef.current = stateCurrentConfigs;
 
@@ -230,7 +120,6 @@ function EmulatorsPage() {
             item.id === 'primehacks' ||
             item.id === 'melonds' ||
             item.id === 'citra' ||
-            item.id === 'rpcs3' ||
             item.id === 'ryujinx' ||
             item.id === 'rmg' ||
             item.id === 'mame' ||
@@ -274,21 +163,6 @@ function EmulatorsPage() {
         });
         i = i + 1;
       });
-      //       ipcChannel.sendMessage('check-versions');
-      //       ipcChannel.once('check-versions', (repoVersions) => {
-      //         // No versioning found, what to do?
-      //         if (repoVersions === '') {
-      //           console.log('no versioning found');
-      //         }
-      //
-      //         const updates = diff(repoVersions, stateCurrentConfigs);
-      //         console.log({ updates });
-      //         setStatePage({
-      //           ...pageRef.current,
-      //           updates: updates,
-      //           newDesiredVersions: repoVersions,
-      //         });
-      //       });
     }, 1000);
   };
 
@@ -306,19 +180,19 @@ function EmulatorsPage() {
   useEffect(() => {
     //Clean win32 systems
 
-    if (system == 'win32') {
-      delete stateCurrentConfigs['primehack'];
-      delete stateCurrentConfigs['melonds'];
-      delete stateCurrentConfigs['citra'];
-      delete stateCurrentConfigs['rpcs3'];
-      delete stateCurrentConfigs['ryujinx'];
-      delete stateCurrentConfigs['rmg'];
-      delete stateCurrentConfigs['mame'];
-      delete stateCurrentConfigs['vita3k'];
-      delete stateCurrentConfigs['scummvm'];
-      delete stateCurrentConfigs['xemu'];
-      delete stateCurrentConfigs['mgba'];
-      delete stateCurrentConfigs['xenia'];
+    if (system === 'win32') {
+      delete stateCurrentConfigs.primehack;
+      delete stateCurrentConfigs.melonds;
+      delete stateCurrentConfigs.citra;
+      delete stateCurrentConfigs.rpcs3;
+      delete stateCurrentConfigs.ryujinx;
+      delete stateCurrentConfigs.rmg;
+      delete stateCurrentConfigs.mame;
+      delete stateCurrentConfigs.vita3k;
+      delete stateCurrentConfigs.scummvm;
+      delete stateCurrentConfigs.xemu;
+      delete stateCurrentConfigs.mgba;
+      delete stateCurrentConfigs.xenia;
     }
 
     ipcChannel.sendMessage('check-versions');
@@ -328,7 +202,7 @@ function EmulatorsPage() {
         console.log('no versioning found');
       }
 
-      //Thanks chatGPT lol
+      // Thanks chatGPT lol
       const obj1 = repoVersions;
       const obj2 = stateCurrentConfigs;
 
@@ -370,8 +244,8 @@ function EmulatorsPage() {
       <Header title="Manage your Emulators" />
       <p className="lead">
         On this page, you can update your configurations or install new
-        emulators. An orange notification means you have a configuration update ready
-        for the respective emulator.
+        emulators. An orange notification means you have a configuration update
+        ready for the respective emulator.
       </p>
       <Notification css={showNotification ? 'is-animated' : 'nope'}>
         {textNotification}
