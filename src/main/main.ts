9569db249b9f55cefa91daf54d781609c1d4e70b
Rodrigo <rodrigosedano@me.com>
2023-05-29 21:14:21 +0200

    Fix launch emus

--- src/main/main.ts
+++ src/main/main.ts
@@ -1097,9 +1097,12 @@ app.on('session-created', (session) => {
 
 ipcMain.on('run-app', async (event, appPath) => {
   let appPathFixed = appPath.replace(/[\r\n]+/g, '');
+  const userFolder = os.homedir();
 
-  appPathFixed = `${os.homedir()}\\emudeck\\EmulationStation-DE\\${appPathFixed}\\`;
-  console.log(appPathFixed);
+  if (appPathFixed.includes('USERPATH')) {
+    appPathFixed = appPathFixed.replace('USERPATH', userFolder);
+  }
+
   let externalApp;
   if (os.platform().includes('win32')) {
     externalApp = spawn(appPathFixed);
