46baabd6e4e1ff2aaf1361e7c3381f83178beeec
Rodrigo <rodrigosedano@me.com>
2026-01-27 20:59:30 +0100

    No android

--- src/renderer/pages/WelcomePage.jsx
+++ src/renderer/pages/WelcomePage.jsx
@@ -1,26 +1,20 @@
-import { useTranslation } from 'react-i18next';
-import React, {
-  useEffect,
-  useState,
-  useContext,
-  useRef,
-  useCallback,
-} from 'react';
-import { GlobalContext } from 'context/globalContext';
-import Wrapper from 'components/molecules/Wrapper/Wrapper';
+import { useTranslation } from "react-i18next";
+import React, { useEffect, useState, useContext, useRef, useCallback } from "react";
+import { GlobalContext } from "context/globalContext";
+import Wrapper from "components/molecules/Wrapper/Wrapper";
 
-import Header from 'components/organisms/Header/Header';
-import Footer from 'components/organisms/Footer/Footer';
-import EmuModal from 'components/molecules/EmuModal/EmuModal';
-import Main from 'components/organisms/Main/Main';
-import ProgressBar from 'components/atoms/ProgressBar/ProgressBar';
-import { useNavigate } from 'react-router-dom';
-import { BtnSimple } from 'getbasecore/Atoms';
-import Card from 'components/molecules/Card/Card';
-import { useFetchCond } from 'hooks/useFetchCond';
+import Header from "components/organisms/Header/Header";
+import Footer from "components/organisms/Footer/Footer";
+import EmuModal from "components/molecules/EmuModal/EmuModal";
+import Main from "components/organisms/Main/Main";
+import ProgressBar from "components/atoms/ProgressBar/ProgressBar";
+import { useNavigate } from "react-router-dom";
+import { BtnSimple } from "getbasecore/Atoms";
+import Card from "components/molecules/Card/Card";
+import { useFetchCond } from "hooks/useFetchCond";
 
 // import { useTranslation } from 'react-i18next';
-import Welcome from 'components/organisms/Wrappers/Welcome';
+import Welcome from "components/organisms/Wrappers/Welcome";
 import {
   iconSuccess,
   iconCloud,
@@ -39,42 +33,31 @@ import {
   iconDisk,
   iconHelp,
   iconScreen,
-} from 'components/utils/images/icons';
+} from "components/utils/images/icons";
 
 function WelcomePage() {
   const { t, i18n } = useTranslation();
   // const { t } = useTranslation();
   const ipcChannel = window.electron.ipcRenderer;
   const { state, setState } = useContext(GlobalContext);
-  const { system, systemName, mode, second, storagePath, gamemode, branch } =
-    state;
+  const { system, systemName, mode, second, storagePath, gamemode, branch } = state;
   const [statePage, setStatePage] = useState({
     disabledNext: true,
     disabledBack: true,
     downloadComplete: !navigator.onLine ? true : null,
     updates: null,
     cloned: null,
-    data: '',
+    data: "",
     modal: undefined,
     dom: undefined,
     news: undefined,
     game_of_the_month: undefined,
   });
-  const {
-    disabledNext,
-    disabledBack,
-    updates,
-    modal,
-    dom,
-    news,
-    game_of_the_month,
-  } = statePage;
+  const { disabledNext, disabledBack, updates, modal, dom, news, game_of_the_month } = statePage;
 
   const navigate = useNavigate();
 
-  const newsWS = useFetchCond(
-    `https://token.emudeck.com/news.php?branch=${branch}`
-  );
+  const newsWS = useFetchCond(`https://token.emudeck.com/news.php?branch=${branch}`);
   useEffect(() => {
     newsWS.post({}).then((data) => {
       setStatePage({
@@ -87,31 +70,27 @@ function WelcomePage() {
 
   const selectMode = (value) => {
     console.log({ value });
-    if (value === 'android') {
-      navigate('/android-welcome');
-    } else {
-      if (value === 'easy' && second === false && system == 'win32') {
-        console.log('esde default');
-        setState({
-          ...state,
-          installFrontends: {
-            ...state.installFrontends,
-            esde: { ...state.installFrontends.esde, status: true },
-            deckyromlauncher: {
-              ...state.installFrontends.deckyromlauncher,
-              status: false,
-            },
+    if (value === "easy" && second === false && system == "win32") {
+      console.log("esde default");
+      setState({
+        ...state,
+        installFrontends: {
+          ...state.installFrontends,
+          esde: { ...state.installFrontends.esde, status: true },
+          deckyromlauncher: {
+            ...state.installFrontends.deckyromlauncher,
+            status: false,
           },
-          mode: value,
-        });
-      } else {
-        console.log('only state');
-        setState({ ...state, mode: value });
-      }
+        },
+        mode: value,
+      });
+    } else {
+      console.log("only state");
+      setState({ ...state, mode: value });
+    }
 
-      if (second) {
-        navigate('/rom-storage');
-      }
+    if (second) {
+      navigate("/rom-storage");
     }
   };
 
@@ -130,38 +109,36 @@ function WelcomePage() {
   // show changelog after update
 
   useEffect(() => {
-    if (systemName === 'ERROR') {
+    if (systemName === "ERROR") {
       const modalData = {
         active: true,
-        header: <span className="h4">{t('WelcomePage.errorModal.title')}</span>,
-        body: t('WelcomePage.errorModal.description'),
-        footer: '',
-        css: 'emumodal--xs',
+        header: <span className="h4">{t("WelcomePage.errorModal.title")}</span>,
+        body: t("WelcomePage.errorModal.description"),
+        footer: "",
+        css: "emumodal--xs",
       };
       setStatePage({ ...statePage, modal: modalData });
     }
   }, [modal]);
 
   useEffect(() => {
-    ipcChannel.sendMessage('check-versions');
-    ipcChannel.once('check-versions', (repoVersions) => {
+    ipcChannel.sendMessage("check-versions");
+    ipcChannel.once("check-versions", (repoVersions) => {
       // No versioning found, what to do?
       console.log({ repoVersions });
-      if (repoVersions === '') {
-        console.log('no versioning found');
+      if (repoVersions === "") {
+        console.log("no versioning found");
       }
-      const currentVersions = JSON.parse(
-        localStorage.getItem('current_versions')
-      );
+      const currentVersions = JSON.parse(localStorage.getItem("current_versions"));
       // If we don't have a previous log of the version, we make the one in the repo the default
       if (!currentVersions) {
         const json = JSON.stringify(repoVersions);
-        localStorage.setItem('current_versions', json);
+        localStorage.setItem("current_versions", json);
       }
 
       if (second === true) {
-        localStorage.setItem('ogStateAlternative', '');
-        localStorage.setItem('ogStateEmus', '');
+        localStorage.setItem("ogStateAlternative", "");
+        localStorage.setItem("ogStateEmus", "");
       }
     });
 
@@ -179,16 +156,14 @@ function WelcomePage() {
     <Wrapper aside={second === true}>
       {second === false && (
         <>
-          <Header title={t('WelcomePage.title', { systemName: systemName })} />
-          <p className="lead">{t('WelcomePage.description')}:</p>
+          <Header title={t("WelcomePage.title", { systemName: systemName })} />
+          <p className="lead">{t("WelcomePage.description")}:</p>
         </>
       )}
 
-      {systemName !== 'ERROR' && second === false && (
-        <Welcome onClick={selectMode} />
-      )}
+      {systemName !== "ERROR" && second === false && <Welcome onClick={selectMode} />}
 
-      {second === false && systemName !== 'ERROR' && (
+      {second === false && systemName !== "ERROR" && (
         <Footer
           back={false}
           next="rom-storage"
@@ -218,12 +193,8 @@ function WelcomePage() {
                 })}
             </div>
             <span className="h2">
-              Games of the month by{' '}
-              <a
-                href="https://retrohandhelds.gg"
-                target="blank"
-                style={{ display: 'inline' }}
-              >
+              Games of the month by{" "}
+              <a href="https://retrohandhelds.gg" target="blank" style={{ display: "inline" }}>
                 <img
                   src="https://retrohandhelds.gg/wp-content/uploads/2023/08/rh_logo_white.svg"
                   alt="RG logo"
